f81cf6caec16e656c1364faab543c6b438b199ff
Dmitrijlin <dmitrij-lin9@rambler.ru>
2017-05-22 15:46:25 +0300

    add services 2

--- src/main/java/models/dmitrij.java
+++ src/main/java/models/dmitrij.java
@@ -4,5 +4,9 @@ package models;
  * Created by Dmitrij on 22.05.2017.
  */
 public class dmitrij {
+<<<<<<< HEAD
     //sdfgsdffgwgfdgdf
+=======
+    //sdfgsdffgwg
+>>>>>>> 4cd8a62... add services 2
 }
